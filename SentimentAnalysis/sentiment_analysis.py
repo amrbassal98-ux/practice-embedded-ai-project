29cae57c899e032bb86526b0f15c3412f5572aea
Amr Bassal <amrbassal98@gmail.com>
2026-05-02 09:10:18 -0400

    Flask server implementation

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -2,14 +2,14 @@
 import requests
 import json
 
-def sentiment_analyzer(test_to_analyse):
+def sentiment_analyzer(text_to_analyse):
     """Analyzes the given text and returns emotion scores."""
     url = (
         "https://sn-watson-sentiment-bert.labs.skills.network"
         "/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
         )
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    json_input = {"raw_document": {"text": test_to_analyse}}
+    json_input = {"raw_document": {"text": text_to_analyse}}
     response = requests.post(url, json = json_input, headers = header, timeout=10)
     formatted_response = json.loads(response.text)
     label = formatted_response['documentSentiment']['label']
