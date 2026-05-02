db9ce64ac4bda533d476820f8a72d9a974ed07a7
Amr Bassal <amrbassal98@gmail.com>
2026-05-02 07:09:58 -0400

    refactor for PEP8 compliance

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,8 +1,13 @@
+"""This module provides a function to analyze emotions using Watson NLP."""
 import requests
 
 def sentiment_analyzer(test_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    """Analyzes the given text and returns emotion scores."""
+    url = (
+        "https://sn-watson-sentiment-bert.labs.skills.network"
+        "/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
+        )
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     json_input = {"raw_document": {"text": test_to_analyse}}
-    response = requests.post(url, json = json_input, headers = header)
+    response = requests.post(url, json = json_input, headers = header, timeout=10)
     return response.text
